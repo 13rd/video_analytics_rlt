73af8f05090114b040bfedf35f7c9056bb9edbf4
Nikitchenko Nikita <nikitos13.n@gmail.com>
2026-02-20 03:13:00 +0400

    create bot on asyncio

---
+++ src/bot/bot.py
@@ -0,0 +1,49 @@
+import asyncio
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import text
+from settings import BOT_TOKEN
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from src.services.llm.llm_service import get_sql_request
+from src.database.session import async_session_maker
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(
+        text="Введите свой запрос по аналитике видео."
+    )
+
+@dp.message()
+async def user_request(message: types.Message):
+    if not message.text or message.text.startswith('/'):
+        return
+
+    query_text = await get_sql_request(user_request=message.text)
+    print(query_text)
+    query: str = text(query_text)
+    data = None
+
+    async with async_session_maker() as session:
+        try:
+            print(query)
+            result = await session.execute(query)
+            data = result.scalar_one_or_none()
+            print(data)
+            await session.commit()
+        except Exception as e:
+            await session.rollback()
+            await message.answer(text=str(e))
+    await message.answer(text=str(data))
+
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
