ed6dcabd39c0de7b48164828958288a2426a32cb
Nikitchenko Nikita <nikitos13.n@gmail.com>
2026-02-20 03:10:26 +0400

    update settings and use settings in all project

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 from envparse import Env
+from dotenv import load_dotenv
 
+load_dotenv()
 env = Env()
 
 DATABASE_URL = env.str(
@@ -7,3 +9,9 @@ DATABASE_URL = env.str(
     default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
 )
 
+BOT_TOKEN = env.str("BOT_TOKEN")
+
+LLM_API_KEY=env.str("LLM_API_KEY")
+LLM_MODEL=env.str("LLM_MODEL", default="deepseek/deepseek-r1-0528:free")
+SITE_URL=env.str("SITE_URL", default="http://localhost:8000")
+SITE_NAME=env.str("SITE_NAME", default="Local SQL Bot")
